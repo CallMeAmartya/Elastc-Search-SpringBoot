0995bce2d7b65055aea9643a5409f951f22741c5
Amartya Sen <amartyasen00077@gmail.com>
2021-08-17 22:20:45 +0530

    Added logstash support

--- src/main/java/com/amartya/elasticsearch/controller/PersonController.java
+++ src/main/java/com/amartya/elasticsearch/controller/PersonController.java
@@ -2,9 +2,11 @@ package com.amartya.elasticsearch.controller;
 
 import com.amartya.elasticsearch.document.Person;
 import com.amartya.elasticsearch.service.PersonService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Log4j2
 @RestController
 @RequestMapping("/api/person")
 public class PersonController {
@@ -13,15 +15,19 @@ public class PersonController {
     @Autowired
     public PersonController(PersonService service) {
         this.service = service;
+        log.info("service started : {}", service);
     }
 
     @PostMapping
     public void save(@RequestBody final Person person) {
         service.save(person);
+        log.info("Person saved : {}", person);
     }
 
     @GetMapping("/{id}")
     public Person findById(@PathVariable final String id) {
-        return service.findById(id);
+        Person person = service.findById(id);
+        log.info("Details retrieved by ID : {}", person);
+        return person;
     }
 }
